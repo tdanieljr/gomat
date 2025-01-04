cb8b283eee3f8580d94e43b5007de725e9642bfc
redek <tdaniel@hey.com>
2025-01-04 10:30:39 -0600

    added initialization ond get/set functionality

--- mat/matrix.go
+++ mat/matrix.go
@@ -3,3 +3,50 @@ package mat
 type Number interface {
 	float64 | complex128
 }
+
+type Matrix[T Number] struct {
+	Data   []T
+	Rows   int
+	Cols   int
+	Stride int
+}
+
+func Init[T Number](rows, cols int) Matrix[T] {
+	data := make([]T, rows*cols)
+	return Matrix[T]{Data: data, Rows: rows, Cols: cols, Stride: cols}
+}
+
+func FromValues[T Number](rows, cols int, data []T) Matrix[T] {
+	if len(data) != rows*cols {
+		panic("mismatch data length and rowxcol count")
+	}
+	return Matrix[T]{Data: data, Rows: rows, Cols: cols, Stride: cols}
+}
+
+func (m Matrix[T]) Get(i, j int) T {
+	return m.Data[i*m.Stride+j]
+}
+
+func (m *Matrix[T]) Set(i, j int, value T) {
+	m.Data[i*m.Stride+j] = value
+}
+
+func (m Matrix[T]) Slice(i, k, j, l int) Matrix[T] {
+	return _slice(m, i, k, j, l, false)
+}
+
+func (m Matrix[T]) SliceWithCopy(i, k, j, l int) Matrix[T] {
+	return _slice(m, i, k, j, l, true)
+}
+
+func _slice[T Number](m Matrix[T], i, k, j, l int, clone bool) Matrix[T] {
+	start := i*m.Stride + j
+	n := (k-1)*m.Stride + l
+
+	if clone {
+		data := make([]T, n)
+		copy(data, m.Data[start:start+n])
+		return Matrix[T]{data, k, l, m.Stride}
+	}
+	return Matrix[T]{m.Data[start : start+n], k, l, m.Stride}
+}
---
+++ mat/matrix_test.go
@@ -0,0 +1,34 @@
+package mat
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestMatrix(t *testing.T) {
+	t.Run("testing init", func(t *testing.T) {
+		got := Init[float64](2, 2)
+		want := Matrix[float64]{[]float64{0, 0, 0, 0}, 2, 2, 2}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("init not as expected")
+		}
+	})
+	t.Run("testing get/set", func(t *testing.T) {
+		m := Init[float64](20, 20)
+		if m.Get(0, 0) != 0 {
+			t.Errorf("Get wrong")
+		}
+		m.Set(3, 7, 100)
+		if m.Get(3, 7) != 100 {
+			t.Errorf("Set wrong")
+		}
+	})
+	t.Run("testing slice", func(t *testing.T) {
+		m := Init[float64](10, 10)
+		m.Set(0, 5, 10)
+		a := m.Slice(0, 3, 2, 7)
+		if a.Get(0, 3) != 10 {
+			t.Errorf("Slice wrong")
+		}
+	})
+}
